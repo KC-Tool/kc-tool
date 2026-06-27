cc57bb89f3755c5f532ce622f562d34feaa1d532
boxiaolanya2008 <3520687734@qq.com>
2026-06-27 18:07:09 +0800

    feat: update color palette and add new colors for Material 3 ADB Optimization Toolbox

--- app/src/main/java/github/boxiaolanya2008/kc_tool/ui/theme/Color.kt
+++ app/src/main/java/github/boxiaolanya2008/kc_tool/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package github.boxiaolanya2008.kc_tool.ui.theme
+﻿package github.boxiaolanya2008.kc_tool.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,25 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Material 3 ADB Optimization Toolbox palette
+val ToolPrimary = Color(0xFF6366F1)
+val ToolOnPrimary = Color(0xFFFFFFFF)
+val ToolPrimaryContainer = Color(0xFFE0E7FF)
+val ToolOnPrimaryContainer = Color(0xFF1E1B4B)
+val ToolSecondary = Color(0xFF6366F1)
+val ToolSecondaryContainer = Color(0xFFEEF2FF)
+val ToolTertiary = Color(0xFF22D3EE)
+val ToolTertiaryContainer = Color(0xFFECFEFF)
+val ToolSurface = Color(0xFF0F172A)
+val ToolSurfaceVariant = Color(0xFF1E293B)
+val ToolSurfaceContainer = Color(0xFF111C8C)
+val ToolSurfaceContainerHigh = Color(0xFF1A26AA)
+val ToolBackground = Color(0xFF0B1222)
+val ToolOnBackground = Color(0xFFE2E8F0)
+val ToolOnSurface = Color(0xFFF8FAFC)
+val ToolOutline = Color(0xFF94A3B8)
+val ToolOutlineVariant = Color(0xFF334155)
+val ToolError = Color(0xFFF87171)
+val ToolSuccess = Color(0xFF34D399)
+val ToolWarning = Color(0xFFFBBF24)
